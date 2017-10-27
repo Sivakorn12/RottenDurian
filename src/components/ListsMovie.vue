cfd84db0163d7f2089784d5b161657db04d585b5
teerarak <fluke.nakub@hotmail.com>
2017-10-27 22:26:51 +0700

    change msg

--- src/components/ListsMovie.vue
+++ src/components/ListsMovie.vue
@@ -13,7 +13,7 @@ export default {
   props: ['movies', 'profile', 'logout', 'updateAddItem', 'updateSubItem'],
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'อย่าบอกใครนะว่าเป็นคนแบบนี้'
     }
   }
 }
